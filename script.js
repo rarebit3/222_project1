b8d274d5ea21a04a559546d02e43f2feecd9aba1
rarebit3 <johntrevorbond@gmail.com>
2022-03-08 15:07:26 -0500

    sterted working on timer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+//Global Variables
+
 let checkArray = []
 let setsFound = 0
-let timer = 0
-
 let deck = [
     {color: 'red', shape: 'oval', number: 1},
     {color: 'red', shape: 'oval', number: 2},
@@ -33,33 +33,96 @@ let deck = [
     ]
 
 let allCards = document.querySelectorAll('div.card')
-// let cardLoc = document.querySelectorAll('div#c1')
+let cardsRemaining = document.querySelector('#cardsRemaining')
+cardsRemaining.innerText = `Cards Remaining: ${deck.length}`
+
+
+//Functions
 
 function dealCard(location) {
-    let index = (Math.floor(Math.random() * deck.length));
-    let card = deck[index];
+    //randomizer
+    let randomIndex = (Math.floor(Math.random() * deck.length));
+    let card = deck[randomIndex];
+    //paste card
     location.innerText = `${card.color}, ${card.shape}, ${card.number}`;
-    deck.splice(index, 1);
+    //deplete deck
+    deck.splice(randomIndex, 1);
+    cardsRemaining.innerText = `Cards Remaining: ${deck.length}`;
     return deck;
+    
 }
+
+
 
 function startGame() {
-    allCards.forEach(e => {dealCard(e)}
-)}
-
-function resetGame() {
-
+    allCards.forEach(e => {dealCard(e)});
+    document.getElementById("idStartGame").disabled = true
+    //TIMER!?
+    // let sec = 0;
+    // function pad ( val ) { return val > 9 ? val : "0" + val; };
+    // let timer = setInterval( function(){
+    //     document.getElementById("seconds").innerHTML=pad(++sec%60);
+    //     document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+    // }, 1000);
 }
 
+function resetGame() {
+    checkArray = [];
+    setsFound = 0
+    deck = [
+    {color: 'red', shape: 'oval', number: 1},
+    {color: 'red', shape: 'oval', number: 2},
+    {color: 'red', shape: 'oval', number: 3},
+    {color: 'red', shape: 'diamond', number: 1},
+    {color: 'red', shape: 'diamond', number: 2},
+    {color: 'red', shape: 'diamond', number: 3},
+    {color: 'red', shape: 'squiggle', number: 1},
+    {color: 'red', shape: 'squiggle', number: 2},
+    {color: 'red', shape: 'squiggle', number: 3},
+    {color: 'green', shape: 'oval', number: 1},
+    {color: 'green', shape: 'oval', number: 2},
+    {color: 'green', shape: 'oval', number: 3},
+    {color: 'green', shape: 'diamond', number: 1},
+    {color: 'green', shape: 'diamond', number: 2},
+    {color: 'green', shape: 'diamond', number: 3},
+    {color: 'green', shape: 'squiggle', number: 1},
+    {color: 'green', shape: 'squiggle', number: 2},
+    {color: 'green', shape: 'squiggle', number: 3},
+    {color: 'purple', shape: 'oval', number: 1},
+    {color: 'purple', shape: 'oval', number: 2},
+    {color: 'purple', shape: 'oval', number: 3},
+    {color: 'purple', shape: 'diamond', number: 1},
+    {color: 'purple', shape: 'diamond', number: 2},
+    {color: 'purple', shape: 'diamond', number: 3},
+    {color: 'purple', shape: 'squiggle', number: 1},
+    {color: 'purple', shape: 'squiggle', number: 2},
+    {color: 'purple', shape: 'squiggle', number: 3}
+    ];
+    allCards.forEach(e => {
+        e.innerText = ""
+            });
+    allCards.forEach(e => {
+        e.setAttribute('style', 'border: 1px solid orange')});
+    document.getElementById("idStartGame").disabled = false;
+    cardsRemaining.innerText = `Cards Remaining: ${deck.length}`;
+    //TRYING TO RESET TIMER
+    // sec = 0;
+    // document.getElementById("seconds").innerHTML= "00";
+    // document.getElementById("minutes").innerHTML= "00";
+    // clearInterval ( timer );
+    
+}
+
+//Event Listeners
 allCards.forEach(e => {
     e.addEventListener('click', function(event) {
         console.log('box clicked', event);
 
         e.setAttribute('style', 'border: 3px solid red')
-        
+        //if selected, deselect
+        //else if array empty, push to checkArray[0]
+        // else if array.length = 1, push to checkArray[1]
+        //else if array.length = 2, push to checkArray[2] and run check winner
+
         })
 })
-
-function checkSet() {
-
-}
