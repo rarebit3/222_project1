9902c7f45292b71cd8f7ad1c53c6e63dbcc86b7c
rarebit3 <johntrevorbond@gmail.com>
2022-03-09 09:26:08 -0500

    added location to objects

--- script.js
+++ script.js
@@ -1,104 +1,79 @@
 //Global Variables
 
-let checkArray = []
-let setsFound = 0
-let deck = [
-    {color: 'red', shape: 'oval', number: 1},
-    {color: 'red', shape: 'oval', number: 2},
-    {color: 'red', shape: 'oval', number: 3},
-    {color: 'red', shape: 'diamond', number: 1},
-    {color: 'red', shape: 'diamond', number: 2},
-    {color: 'red', shape: 'diamond', number: 3},
-    {color: 'red', shape: 'squiggle', number: 1},
-    {color: 'red', shape: 'squiggle', number: 2},
-    {color: 'red', shape: 'squiggle', number: 3},
-    {color: 'green', shape: 'oval', number: 1},
-    {color: 'green', shape: 'oval', number: 2},
-    {color: 'green', shape: 'oval', number: 3},
-    {color: 'green', shape: 'diamond', number: 1},
-    {color: 'green', shape: 'diamond', number: 2},
-    {color: 'green', shape: 'diamond', number: 3},
-    {color: 'green', shape: 'squiggle', number: 1},
-    {color: 'green', shape: 'squiggle', number: 2},
-    {color: 'green', shape: 'squiggle', number: 3},
-    {color: 'purple', shape: 'oval', number: 1},
-    {color: 'purple', shape: 'oval', number: 2},
-    {color: 'purple', shape: 'oval', number: 3},
-    {color: 'purple', shape: 'diamond', number: 1},
-    {color: 'purple', shape: 'diamond', number: 2},
-    {color: 'purple', shape: 'diamond', number: 3},
-    {color: 'purple', shape: 'squiggle', number: 1},
-    {color: 'purple', shape: 'squiggle', number: 2},
-    {color: 'purple', shape: 'squiggle', number: 3}
+const gameDeck = [
+    {location: '', color: 'red', shape: 'oval', number: 1},
+    {location: '', color: 'red', shape: 'oval', number: 2},
+    {location: '', color: 'red', shape: 'oval', number: 3},
+    {location: '', color: 'red', shape: 'diamond', number: 1},
+    {location: '', color: 'red', shape: 'diamond', number: 2},
+    {location: '', color: 'red', shape: 'diamond', number: 3},
+    {location: '', color: 'red', shape: 'squiggle', number: 1},
+    {location: '', color: 'red', shape: 'squiggle', number: 2},
+    {location: '', color: 'red', shape: 'squiggle', number: 3},
+    {location: '', color: 'green', shape: 'oval', number: 1},
+    {location: '', color: 'green', shape: 'oval', number: 2},
+    {location: '', color: 'green', shape: 'oval', number: 3},
+    {location: '', color: 'green', shape: 'diamond', number: 1},
+    {location: '', color: 'green', shape: 'diamond', number: 2},
+    {location: '', color: 'green', shape: 'diamond', number: 3},
+    {location: '', color: 'green', shape: 'squiggle', number: 1},
+    {location: '', color: 'green', shape: 'squiggle', number: 2},
+    {location: '', color: 'green', shape: 'squiggle', number: 3},
+    {location: '', color: 'purple', shape: 'oval', number: 1},
+    {location: '', color: 'purple', shape: 'oval', number: 2},
+    {location: '', color: 'purple', shape: 'oval', number: 3},
+    {location: '', color: 'purple', shape: 'diamond', number: 1},
+    {location: '', color: 'purple', shape: 'diamond', number: 2},
+    {location: '', color: 'purple', shape: 'diamond', number: 3},
+    {location: '', color: 'purple', shape: 'squiggle', number: 1},
+    {location: '', color: 'purple', shape: 'squiggle', number: 2},
+    {location: '', color: 'purple', shape: 'squiggle', number: 3}
     ]
-
+let deck = gameDeck
+let checkArray = []
+let placedCards = []
+let selectedCards = []
+let setsFound = 0
 let allCards = document.querySelectorAll('div.card')
 let cardsRemaining = document.querySelector('#cardsRemaining')
 cardsRemaining.innerText = `Cards Remaining: ${deck.length}`
-
-
-//Functions
-
-function dealCard(location) {
-    //randomizer
-    let randomIndex = (Math.floor(Math.random() * deck.length));
-    let card = deck[randomIndex];
-    //paste card
-    location.innerText = `${card.color}, ${card.shape}, ${card.number}`;
-    //deplete deck
-    deck.splice(randomIndex, 1);
-    cardsRemaining.innerText = `Cards Remaining: ${deck.length}`;
-    console.log(location)
-    return deck;
-    
-}
-
 
 
 function startGame() {
     allCards.forEach(e => {dealCard(e)});
     document.getElementById("idStartGame").disabled = true
     //TIMER!?
-    let sec = 0;
-    function pad ( val ) { return val > 9 ? val : "0" + val; };
-    let timer = setInterval( function(){
-        document.getElementById("seconds").innerHTML=pad(++sec%60);
-        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
-    }, 1000);
+//     let sec = 0;
+//     function pad ( val ) { return val > 9 ? val : "0" + val; };
+//     let timer = setInterval( function(){
+//         document.getElementById("seconds").innerHTML=pad(++sec%60);
+//         document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+//     }, 1000);
+}
+
+function dealCard(divLocation) {
+    //randomizer
+    let randomIndex = (Math.floor(Math.random() * deck.length));
+    let card = deck[randomIndex];
+    //paste card and link location
+    card.location = divLocation.id
+    divLocation.innerText = `${card.color}, ${card.shape}, ${card.number}, ${card.location}`;
+    //deplete deck
+    placedCards.push(card)
+    deck.splice(randomIndex, 1);
+    cardsRemaining.innerText = `Cards Remaining: ${deck.length}`;
+    //console.log(placedCards)
+    return deck;
+    
 }
 
 function resetGame() {
+    //All the things become 0
     checkArray = [];
-    setsFound = 0
-    deck = [
-    {color: 'red', shape: 'oval', number: 1},
-    {color: 'red', shape: 'oval', number: 2},
-    {color: 'red', shape: 'oval', number: 3},
-    {color: 'red', shape: 'diamond', number: 1},
-    {color: 'red', shape: 'diamond', number: 2},
-    {color: 'red', shape: 'diamond', number: 3},
-    {color: 'red', shape: 'squiggle', number: 1},
-    {color: 'red', shape: 'squiggle', number: 2},
-    {color: 'red', shape: 'squiggle', number: 3},
-    {color: 'green', shape: 'oval', number: 1},
-    {color: 'green', shape: 'oval', number: 2},
-    {color: 'green', shape: 'oval', number: 3},
-    {color: 'green', shape: 'diamond', number: 1},
-    {color: 'green', shape: 'diamond', number: 2},
-    {color: 'green', shape: 'diamond', number: 3},
-    {color: 'green', shape: 'squiggle', number: 1},
-    {color: 'green', shape: 'squiggle', number: 2},
-    {color: 'green', shape: 'squiggle', number: 3},
-    {color: 'purple', shape: 'oval', number: 1},
-    {color: 'purple', shape: 'oval', number: 2},
-    {color: 'purple', shape: 'oval', number: 3},
-    {color: 'purple', shape: 'diamond', number: 1},
-    {color: 'purple', shape: 'diamond', number: 2},
-    {color: 'purple', shape: 'diamond', number: 3},
-    {color: 'purple', shape: 'squiggle', number: 1},
-    {color: 'purple', shape: 'squiggle', number: 2},
-    {color: 'purple', shape: 'squiggle', number: 3}
-    ];
+    placedCards = [];
+    selectedCards = [];
+    setsFound = 0;
+    deck = gameDeck
     allCards.forEach(e => {
         e.innerText = ""
             });
@@ -111,23 +86,21 @@ function resetGame() {
     // document.getElementById("seconds").innerHTML= "00";
     // document.getElementById("minutes").innerHTML= "00";
     // clearInterval ( timer );
+}
+
+function selectCard(event) {
+    selectedCards.push(placedCards[this.id])
+    console.log(selectedCards)
+
+    // let card = cardSlotId.parseInt
+    // selectedCards.push(selectedCards[card])
+
     
 }
 
+
 //Event Listeners
-allCards.forEach(e => {
-    e.addEventListener('click', function(event) {
-        console.log('box clicked', event);
-        
-        e.setAttribute('style', 'border: 3px solid red')
-        //if selected, deselect
-        //else if array empty, push to checkArray[0]
-        // else if array.length = 1, push to checkArray[1]
-        //else if array.length = 2, push to checkArray[2] and run check winner
-
-        })
-})
-
+allCards.forEach(e => {e.addEventListener('click', selectCard)})
 
 //WIN LOGIC PSUEDO-CODE
 // let colorSet = false
@@ -148,5 +121,4 @@ allCards.forEach(e => {
 //     dealCard(to empty locations)
 
 // } else {
-//     deslectCards()
-// }
+//     deslectCards(
