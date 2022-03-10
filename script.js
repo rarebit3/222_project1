d96fd59d6d9b35315c725bd9c55536c2e79a66a4
rarebit3 <johntrevorbond@gmail.com>
2022-03-10 11:48:51 -0500

    current state

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
 //Global Variables
 const gameDeck = [
-    {selected: false,location: '', color: 'red', shape: 'oval', number: 1},
-    {selected: false,location: '', color: 'red', shape: 'oval', number: 2},
-    {selected: false,location: '', color: 'red', shape: 'oval', number: 3},
-    {selected: false,location: '', color: 'red', shape: 'diamond', number: 1},
-    {selected: false,location: '', color: 'red', shape: 'diamond', number: 2},
-    {selected: false,location: '', color: 'red', shape: 'diamond', number: 3},
-    {selected: false,location: '', color: 'red', shape: 'squiggle', number: 1},
-    {selected: false,location: '', color: 'red', shape: 'squiggle', number: 2},
-    {selected: false,location: '', color: 'red', shape: 'squiggle', number: 3},
-    {selected: false,location: '', color: 'green', shape: 'oval', number: 1},
-    {selected: false,location: '', color: 'green', shape: 'oval', number: 2},
-    {selected: false,location: '', color: 'green', shape: 'oval', number: 3},
-    {selected: false,location: '', color: 'green', shape: 'diamond', number: 1},
-    {selected: false,location: '', color: 'green', shape: 'diamond', number: 2},
-    {selected: false,location: '', color: 'green', shape: 'diamond', number: 3},
-    {selected: false,location: '', color: 'green', shape: 'squiggle', number: 1},
-    {selected: false,location: '', color: 'green', shape: 'squiggle', number: 2},
-    {selected: false,location: '', color: 'green', shape: 'squiggle', number: 3},
-    {selected: false,location: '', color: 'purple', shape: 'oval', number: 1},
-    {selected: false,location: '', color: 'purple', shape: 'oval', number: 2},
-    {selected: false,location: '', color: 'purple', shape: 'oval', number: 3},
-    {selected: false,location: '', color: 'purple', shape: 'diamond', number: 1},
-    {selected: false,location: '', color: 'purple', shape: 'diamond', number: 2},
-    {selected: false,location: '', color: 'purple', shape: 'diamond', number: 3},
-    {selected: false,location: '', color: 'purple', shape: 'squiggle', number: 1},
-    {selected: false,location: '', color: 'purple', shape: 'squiggle', number: 2},
-    {selected: false,location: '', color: 'purple', shape: 'squiggle', number: 3}
+    {selected: false, location: '', color: 'red', shape: 'oval', number: 1},
+    {selected: false, location: '', color: 'red', shape: 'oval', number: 2},
+    {selected: false, location: '', color: 'red', shape: 'oval', number: 3},
+    {selected: false, location: '', color: 'red', shape: 'diamond', number: 1},
+    {selected: false, location: '', color: 'red', shape: 'diamond', number: 2},
+    {selected: false, location: '', color: 'red', shape: 'diamond', number: 3},
+    {selected: false, location: '', color: 'red', shape: 'squiggle', number: 1},
+    {selected: false, location: '', color: 'red', shape: 'squiggle', number: 2},
+    {selected: false, location: '', color: 'red', shape: 'squiggle', number: 3},
+    {selected: false, location: '', color: 'green', shape: 'oval', number: 1},
+    {selected: false, location: '', color: 'green', shape: 'oval', number: 2},
+    {selected: false, location: '', color: 'green', shape: 'oval', number: 3},
+    {selected: false, location: '', color: 'green', shape: 'diamond', number: 1},
+    {selected: false, location: '', color: 'green', shape: 'diamond', number: 2},
+    {selected: false, location: '', color: 'green', shape: 'diamond', number: 3},
+    {selected: false, location: '', color: 'green', shape: 'squiggle', number: 1},
+    {selected: false, location: '', color: 'green', shape: 'squiggle', number: 2},
+    {selected: false, location: '', color: 'green', shape: 'squiggle', number: 3},
+    {selected: false, location: '', color: 'purple', shape: 'oval', number: 1},
+    {selected: false, location: '', color: 'purple', shape: 'oval', number: 2},
+    {selected: false, location: '', color: 'purple', shape: 'oval', number: 3},
+    {selected: false, location: '', color: 'purple', shape: 'diamond', number: 1},
+    {selected: false, location: '', color: 'purple', shape: 'diamond', number: 2},
+    {selected: false, location: '', color: 'purple', shape: 'diamond', number: 3},
+    {selected: false, location: '', color: 'purple', shape: 'squiggle', number: 1},
+    {selected: false, location: '', color: 'purple', shape: 'squiggle', number: 2},
+    {selected: false, location: '', color: 'purple', shape: 'squiggle', number: 3}
     ]
 let deck = gameDeck;
-let checkArray = [];
 let placedCards = [];
 let selectedCards = [];
 let setsFound = 0;
 let allCardSlots = document.querySelectorAll("div.card");
 let cardsRemaining = document.querySelector("#cardsRemaining");
 cardsRemaining.innerText = `Cards Remaining: ${deck.length}`;
+let numberCheck = false
+let shapeCheck = false
+let colorCheck = false
+
 
 
 function startGame() {
@@ -43,13 +46,6 @@ function startGame() {
     dealCard(e);
   });
   document.getElementById("idStartGame").disabled = true;
-  //TIMER!?
-  //     let sec = 0;
-  //     function pad ( val ) { return val > 9 ? val : "0" + val; };
-  //     let timer = setInterval( function(){
-  //         document.getElementById("seconds").innerHTML=pad(++sec%60);
-  //         document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
-  //     }, 1000);
 }
 
 function dealCard(divLocation) {
@@ -69,42 +65,43 @@ function dealCard(divLocation) {
 
 function resetGame() {
   //All the things become 0
-  checkArray = [];
-  placedCards = [];
-  selectedCards = [];
-  setsFound = 0;
-  deck = gameDeck;
-  allCardSlots.forEach((e) => {
+    placedCards = [];
+    selectedCards = [];
+    setsFound = 0;
+    deck = gameDeck;
+    allCardSlots.forEach((e) => {
     e.innerText = "";
   });
-  allCardSlots.forEach((e) => {
-    e.setAttribute("style", "border: 1px solid orange");
+    document.querySelectorAll(".highlight").forEach((e) => {
+    e.classList.remove("highlight");
   });
-  document.getElementById("idStartGame").disabled = false;
-  cardsRemaining.innerText = `Cards Remaining: ${deck.length}`;
-  //TRYING TO RESET TIMER
-  // sec = 0;
-  // document.getElementById("seconds").innerHTML= "00";
-  // document.getElementById("minutes").innerHTML= "00";
-  // clearInterval ( timer );
+    document.getElementById("idStartGame").disabled = false;
+    cardsRemaining.innerText = `Cards Remaining: ${deck.length}`;
 }
 
 
 
 allCardSlots.forEach((e, divLocation) => {
-  e.addEventListener("click", function () {
+  e.addEventListener("click", function clickDiv() {
     e.selected = !e.selected;
-    selectedCards.push(placedCards[divLocation]);
     e.selected ? e.classList.add("highlight") : e.classList.remove("highlight");
+    selectedCards.push(placedCards[divLocation]);
     console.log();
     if (selectedCards.length > 2) {
-      checkColor(selectedCards);
-      checkShape(selectedCards);
-      checkNumber(selectedCards);
-    }
+        checkSet(selectedCards);
+       }
+    
   });
+  
 });
 
+function checkSet() {
+    if (checkColor(selectedCards) && checkShape(selectedCards) && checkNumber(selectedCards)  == true) {
+        console.log('set found')
+
+    } else {console.log('borken')}
+    
+}
 
 
 function checkColor(n) {
@@ -117,9 +114,9 @@ function checkShape(n) {
     else if (n[0].shape != n[1].shape && n[0].shape != n[2].shape && n[1].shape != n[2].shape) return true;
     else return false;
      }
-function checkColor(n) {
+function checkNumber(n) {
     if (n[0].number == n[1].number == n[2].number) return true;
     else if (n[0].number != n[1].number && n[0].number != n[2].number && n[1].number != n[2].number) return true;
     else return false;
      }
-     
+
